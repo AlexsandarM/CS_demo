1e4f0f7e294908c37bb6e105828f3d28ca17e81a
Aleksandar Mitrevski <AlexsandarMSN@hotmail.com>
2020-02-13 11:03:04 +0100

    Individual project

--- ProductsApi/ProductsApi/ProductsApi/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace ProductsApi.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- ProductsApi/ProductsApi/ProductsApi/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace ProductsApi.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
